985bdf962573cb74957c4e66a03de65005d0b014
Thomas Gillespie <s1911520@ed.ac.uk>
2022-03-28 16:09:42 +0100

    Implemented controller package

--- src/main/java/controller/Context.java
+++ src/main/java/controller/Context.java
@@ -1,4 +1,44 @@
 package controller;
 
+import external.MockPaymentSystem;
+import external.PaymentSystem;
+import state.*;
+
 public class Context {
+    private PaymentSystem paymentSystem;
+    private IUserState userState;
+    private IEventState eventState;
+    private IBookingState bookingState;
+    private ISponsorshipState sponsorshipState;
+    public Context(){
+        this.paymentSystem = new MockPaymentSystem();
+        this.userState = new UserState();
+        this.eventState = new EventState();
+        this.bookingState = new BookingState();
+        this.sponsorshipState = new SponsorshipState();
+    }
+    public Context(Context other){
+        this.userState = new UserState(other.getUserState());
+        this.eventState = new EventState(other.getEventState());
+        this.bookingState = new BookingState(other.getBookingState());
+        this.sponsorshipState = new SponsorshipState(other.getSponsorshipState());
+        // Check shallow reference cloning here:
+        PaymentSystem reference = other.getPaymentSystem();
+        this.paymentSystem = reference;
+    }
+    public PaymentSystem getPaymentSystem(){
+        return paymentSystem;
+    }
+    public IUserState getUserState(){
+        return userState;
+    }
+    public IBookingState getBookingState(){
+        return bookingState;
+    }
+    public IEventState getEventState(){
+        return eventState;
+    }
+    public ISponsorshipState getSponsorshipState(){
+        return sponsorshipState;
+    }
 }
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,14 @@
 package controller;
 
+import command.ICommand;
+
 public class Controller {
+    private Context keptContext;
+    public Controller(){
+        Context newContext = new Context();
+        this.keptContext = newContext;
+    }
+    public void runCommand(ICommand command){
+        command.execute(keptContext);
+    }
 }
