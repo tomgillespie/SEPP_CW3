02cbeb71e3e5c4c4ab045a204866f50bb7810f87
Thomas Gillespie <s1911520@ed.ac.uk>
2022-03-31 15:10:53 +0100

    For peer review

--- src/main/java/external/MockPaymentSystem.java
+++ src/main/java/external/MockPaymentSystem.java
@@ -1,9 +1,35 @@
 package external;
 
+import java.util.List;
+
 public class MockPaymentSystem implements PaymentSystem{
     private String buyerAccountEmail;
     private String sellerAccountEmail;
     private double transactionAmount;
+    private List<Transaction> transactionList;
+
+    public class Transaction {
+        private String buyerAccountEmail;
+        private String sellerAccountEmail;
+        private double transactionAmount;
+
+        public Transaction(String buyerAccountEmail, String sellerAccountEmail, double transactionAmount){
+            this.buyerAccountEmail = buyerAccountEmail;
+            this.sellerAccountEmail = sellerAccountEmail;
+            this.transactionAmount = transactionAmount;
+        }
+
+        @Override
+        public boolean equals(Object obj)
+        {
+            return true;
+        }
+
+        @Override
+        public int hashCode(){
+            return 1;
+        }
+    }
 
     public MockPaymentSystem(){
     }
